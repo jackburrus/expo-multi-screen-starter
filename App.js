ab852e451efa643cf2654172076d380898b1d1fe
Jack Burrus <jhburrus3@gmail.com>
2020-02-06 19:00:58 -0500

    Changed app.js to functional component

--- App.js
+++ App.js
@@ -1,75 +1,25 @@
-import React from 'react';
-import { StatusBar, View } from 'react-native';
-import { AppLoading, ScreenOrientation } from 'expo';
-import { Appearance } from 'react-native-appearance';
-import { device, func, gStyle } from './src/constants';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { func } from './src/constants/functions';
 
-// tab navigator
-import TabNavigator from './src/navigation/TabNavigator';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoading: true,
-      theme: 'light'
-    };
-
-    // is iPad?
-    if (device.isPad) {
-      ScreenOrientation.allowAsync(
-        ScreenOrientation.Orientation.ALL_BUT_UPSIDE_DOWN
-      );
-    }
-
-    this.updateTheme = this.updateTheme.bind(this);
-  }
-
-  componentDidMount() {
-    // get system preference
-    const colorScheme = Appearance.getColorScheme();
-
-    // if light or dark
-    if (colorScheme !== 'no-preference') {
-      this.setState({
-        theme: colorScheme
-      });
-    }
-  }
-
-  updateTheme(themeType) {
-    this.setState({
-      theme: themeType
-    });
-  }
-
-  render() {
-    const { isLoading, theme } = this.state;
-    const iOSStatusType = theme === 'light' ? 'dark-content' : 'light-content';
-
-    if (isLoading) {
-      return (
-        <AppLoading
-          onFinish={() => this.setState({ isLoading: false })}
-          startAsync={func.loadAssetsAsync}
-        />
-      );
-    }
-
-    return (
-      <View style={gStyle.container[theme]}>
-        <StatusBar barStyle={device.iOS ? iOSStatusType : 'light-content'} />
-
-        <TabNavigator
-          screenProps={{
-            updateTheme: this.updateTheme
-          }}
-          theme={theme}
-        />
-      </View>
-    );
-  }
+export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  return isLoading === false ? (
+    <AppLoading startAsync={func.loadAssetsAsync} />
+  ) : (
+    <View style={styles.container}>
+      <Text>Hi Jack! Get to work.</Text>
+      <Text>Make sure you finish this one.</Text>
+    </View>
+  );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
